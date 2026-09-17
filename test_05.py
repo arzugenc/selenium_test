3c56f0b06f5df77a3c33a223c3492687057f1517
Arzu Genç <arzugenc2025@gmail.com>
2026-09-17 11:17:47 +0300

    add click action

--- test_05.py
+++ test_05.py
@@ -13,6 +13,14 @@ site_url="https://tr.wikipedia.org/wiki/Anasayfa"
 driver.get(site_url)
 sleep(2)
 
+"""
+driver.find_element(By.XPATH,'//*[@id="mp-itn"]/ul/li[3]').click()
+
+"""
+
+search_box=driver.find_element(By.CLASS_NAME,"cdx-text-input__input")
+search_box.send_keys("Adnan Menderes") 
+sleep(2)
 driver.find_element(By.XPATH,'//*[@id="mp-itn"]/ul/li[3]').click()
 sleep(2)
 

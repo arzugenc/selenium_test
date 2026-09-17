fc6ae0482282aefc0fe9f783c5a8a4bde61d7ca6
Arzu Genç <arzugenc2025@gmail.com>
2026-09-17 12:29:54 +0300

    add secroll

--- test_06.py
+++ test_06.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import chromedriver_autoinstaller
 from time import sleep
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.keys import Keys
 chromedriver_autoinstaller.install()
 
 driver=webdriver.Chrome()
@@ -13,7 +13,15 @@ site_url="https://tr.wikipedia.org/wiki/Anasayfa"
 driver.get(site_url)
 sleep(2)
 
-driver.execute_script("window.scrollTo(0,document.documentElement.scrollHeight);")
+"""driver.execute_script("window.scrollTo(0,document.documentElement.scrollHeight);")"""
+"""driver.execute_script("window.scrollTo(document.documentElement.scrollHeight,0);")"""
+
+driver.execute_script("window.scrollTo(0,800);")
+
+"""driver.find_element(By.TAG_NAME,'body').send_keys(Keys.END)"""
+
+
+
 sleep(3)
 
 driver.quit()
